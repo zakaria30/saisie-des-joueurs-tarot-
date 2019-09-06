ae73f4609624035451755f536416b47bcfd8d736
bousmaha <bousmaha@A17>
2019-09-06 15:10:15 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleApplication1/Source.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+#include <ctime>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+	 
+ string joueur1; 
+ string joueur2;
+ string joueur3;
+ string joueur4;
+
+ cout << "Joueur 1" << endl << endl;
+ cin >> joueur1;
+ cout << endl;
+
+ cout << "Joueur 2" << endl << endl;
+ cin >> joueur2;
+ cout << endl;
+
+ cout << "Joueur 3" << endl << endl;
+ cin >> joueur3;
+ cout << endl;
+
+ cout << "Joueur 4" << endl << endl;
+ cin >> joueur4;
+ cout << endl;
+ cout << endl;
+
+ printf("liste des joueurs");
+ cout << endl;
+ cout << endl;
+ cout << joueur1;
+ cout << endl;
+ cout << endl;
+ cout << joueur2;
+ cout << endl;
+ cout << endl;
+ cout << joueur3;
+ cout << endl;
+ cout << endl;
+ cout << joueur4;
+ cout << endl;
+ cout << endl;
+
+ return 0;
+}
